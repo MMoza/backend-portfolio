b420fb0a2233db9e472cc827cc9504c9be249dd7
m.moza <m.moza@mplan.es>
2026-03-05 18:30:26 +0100

    init portfolio astro

---
+++ tailsiwnd.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+
+  darkMode: "class",
+
+  content: [
+    "./src/**/*.{astro,html,js,ts,jsx,tsx}"
+  ],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [],
+};
